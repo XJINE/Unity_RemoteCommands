fb710355a6c64cc81b5122a36675318d696b10f6
XJINE <>
2025-07-05 07:58:37 +0900

    Update logging logic and accessibility.

--- Assets/Packages/RemoteCommands/RemoteCommand.cs
+++ Assets/Packages/RemoteCommands/RemoteCommand.cs
@@ -2,73 +2,71 @@
 using System.Reflection;
 using UnityEngine;
 
-namespace RemoteCommands
+namespace RemoteCommands {
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Field | AttributeTargets.Property, Inherited = false)]
+public class RemoteCommand : Attribute
 {
-    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Field | AttributeTargets.Property, Inherited = false)]
-    public class RemoteCommand : Attribute
+    #region Property
+
+    // CAUTION:
+    // ID must be unique.
+
+    private string _id;
+    public string ID
     {
-        #region Property
-
-        // CAUTION:
-        // ID must be unique.
-
-        private string _id;
-        public string ID
-        {
-            get => _id;
-            set { if (!IsInitialized) { _id = value; } }
-        }
-
-        public    bool          IsInitialized { get; protected set; }
-        protected MonoBehaviour Instance      { get;           set; }
-        protected MemberInfo    MemberInfo    { get;           set; }
-
-        #endregion Property
-
-        #region Method
-
-        public bool Initialize(MonoBehaviour instance, MemberInfo memberInfo)
-        {
-            if (IsInitialized)
-            {
-                return false;
-            }
-
-            ID ??= memberInfo.Name;
-
-            Instance      = instance;
-            MemberInfo    = memberInfo;
-            IsInitialized = true;
-
-            return true;
-        }
-
-        public object Invoke(params object[] parameters)
-        {
-            switch (MemberInfo.MemberType)
-            {
-                case MemberTypes.Field:
-                    var fieldInfo = ((FieldInfo)MemberInfo);
-                        fieldInfo.SetValue(Instance, parameters[0]);
-                 return fieldInfo.GetValue(Instance);
-
-                case MemberTypes.Property:
-                    var propertyInfo = ((PropertyInfo)MemberInfo);
-                        propertyInfo.SetValue(Instance, parameters[0]);
-                 return propertyInfo.GetValue(Instance);
-                
-                case MemberTypes.Method:
-                    return ((MethodInfo) MemberInfo).Invoke(Instance, parameters);
-            }
-
-            return Instance;
-        }
-
-        public override string ToString()
-        {
-            return MemberInfo.ReflectedType.Name + "." + MemberInfo.Name;;
-        }
-
-        #endregion Method
+        get => _id;
+        set { if (!IsInitialized) { _id = value; } }
     }
-}
+
+    public bool          IsInitialized { get; private set; }
+    public MonoBehaviour Instance      { get; private set; }
+    public MemberInfo    MemberInfo    { get; private set; }
+
+    #endregion Property
+
+    #region Method
+
+    public bool Initialize(MonoBehaviour instance, MemberInfo memberInfo)
+    {
+        if (IsInitialized)
+        {
+            return false;
+        }
+
+        ID ??= memberInfo.Name;
+
+        Instance      = instance;
+        MemberInfo    = memberInfo;
+        IsInitialized = true;
+
+        return true;
+    }
+
+    public object Invoke(params object[] parameters)
+    {
+        switch (MemberInfo.MemberType)
+        {
+            case MemberTypes.Field:
+                var fieldInfo = ((FieldInfo)MemberInfo);
+                    fieldInfo.SetValue(Instance, parameters[0]);
+             return fieldInfo.GetValue(Instance);
+
+            case MemberTypes.Property:
+                var propertyInfo = ((PropertyInfo)MemberInfo);
+                    propertyInfo.SetValue(Instance, parameters[0]);
+             return propertyInfo.GetValue(Instance);
+            
+            case MemberTypes.Method:
+                return ((MethodInfo) MemberInfo).Invoke(Instance, parameters);
+        }
+
+        return Instance;
+    }
+
+    public override string ToString()
+    {
+        return MemberInfo.ReflectedType.Name + "." + MemberInfo.Name;
+    }
+
+    #endregion Method
+}}
--- Assets/Packages/RemoteCommands/RemoteCommander.cs
+++ Assets/Packages/RemoteCommands/RemoteCommander.cs
@@ -5,141 +5,121 @@ using System.Linq;
 using System.Reflection;
 using UnityEngine;
 
-namespace RemoteCommands
+namespace RemoteCommands {
+public class RemoteCommander : SingletonMonoBehaviour<RemoteCommander>, IInitializable
 {
-    public class RemoteCommander : SingletonMonoBehaviour<RemoteCommander>, IInitializable
+    #region Field
+
+    private Dictionary<string, RemoteCommand> _commands;
+
+    #endregion Field
+
+    #region Property
+
+    public bool IsInitialized { get; protected set; }
+
+    public ReadOnlyDictionary<string, RemoteCommand> Commands { get; protected set; }
+
+    #endregion Property
+
+    #region Method
+
+    protected virtual void Start()
     {
-        #region Field
-
-        private Dictionary<string, RemoteCommand> _commands;
-
-        #endregion Field
-
-        #region Property
-
-        public bool IsInitialized { get; protected set; }
-
-        public ReadOnlyDictionary<string, RemoteCommand> Commands { get; protected set; }
-
-        #endregion Property
-
-        #region Method
-
-        protected virtual void Start()
-        {
-            // CAUTION:
-            // To scan other objects, Initialize shouldn't be called in Awake.
-            Initialize();
-        }
-
-        public virtual bool Initialize()
-        {
-            if (IsInitialized)
-            {
-                return false;
-            }
-
-            IsInitialized = true;
-
-            _commands = new Dictionary<string, RemoteCommand>();
-            Commands  = new ReadOnlyDictionary<string, RemoteCommand>(_commands);
-
-            foreach (var monoBehaviour in FindObjectsOfType<MonoBehaviour>())
-            { 
-                var type = monoBehaviour.GetType();
-
-                while (type != null)
-                {
-                    FindCommands(monoBehaviour, type);
-                    type = type.BaseType;
-                }
-            }
-
-            return true;
-        }
-
-        protected void FindCommands(MonoBehaviour instance, Type type)
-        {
-            // NOTE:
-            // DeclaredOnly needs to avoid conflict in inherit class.
-
-            var memberInfos = type.GetMembers(BindingFlags.Public
-                                            | BindingFlags.NonPublic
-                                            | BindingFlags.Instance
-                                            | BindingFlags.Static
-                                            | BindingFlags.DeclaredOnly);
-
-            foreach (var methodInfo in memberInfos)
-            {
-                var remoteCommand = Attribute.GetCustomAttribute(methodInfo, typeof(RemoteCommand)) as RemoteCommand;
-
-                if (remoteCommand == null)
-                {
-                    continue;
-                }
-
-                remoteCommand.Initialize(instance, methodInfo);
-
-                if (!_commands.TryAdd(remoteCommand.ID, remoteCommand))
-                {
-                    AlertIDConflict(_commands[remoteCommand.ID], remoteCommand);
-                }
-            }
-        }
-
-        public virtual object Command(params object[] parameters)
-        {
-            var id = parameters[0].ToString();
-
-            if (!_commands.TryGetValue(id, out var command))
-            {
-                AlertCommandNotFound(id);
-                return null;
-            }
-            try
-            {
-                parameters = parameters.Skip(1).ToArray();
-                return command.Invoke(parameters);
-            }
-            catch (Exception exception)
-            {
-                AlertInvokeError(command, exception);
-                return null;
-            }
-        }
-
-        public bool AddCommand(RemoteCommand command)
-        {
-            if (!command.IsInitialized)
-            {
-                Debug.Log("command is not initialized.");
-                return false;
-            }
-
-            if (!_commands.TryAdd(command.ID, command))
-            {
-                AlertIDConflict(_commands[command.ID], command);
-                return false;
-            }
-
-            return true;
-        }
-
-        protected virtual void AlertIDConflict(RemoteCommand existingCommand, RemoteCommand conflictCommand)
-        {
-            Debug.Log("Conflict Command ID: " + existingCommand.ID + ", " + existingCommand + ", " + conflictCommand);
-        }
-
-        protected virtual void AlertCommandNotFound(string id)
-        {
-            Debug.Log("Command ID '" + id + "' is not found.");
-        }
-
-        protected virtual void AlertInvokeError(RemoteCommand command, Exception exception)
-        {
-            Debug.Log("Invoke " + command + " is failed. " + exception);
-        }
-
-        #endregion Method
+        // CAUTION:
+        // To scan other objects, Initialize shouldn't be called in Awake.
+        Initialize();
     }
-}
+
+    public virtual bool Initialize()
+    {
+        if (IsInitialized)
+        {
+            return false;
+        }
+
+        IsInitialized = true;
+
+        _commands = new Dictionary        <string, RemoteCommand>();
+        Commands  = new ReadOnlyDictionary<string, RemoteCommand>(_commands);
+
+        foreach (var monoBehaviour in FindObjectsOfType<MonoBehaviour>(includeInactive:true))
+        { 
+            var type = monoBehaviour.GetType();
+
+            while (type != null)
+            {
+                FindCommands(monoBehaviour, type);
+                type = type.BaseType;
+            }
+        }
+
+        return true;
+    }
+
+    private void FindCommands(MonoBehaviour instance, IReflect type)
+    {
+        // NOTE:
+        // DeclaredOnly needs to avoid conflict in inherit class.
+
+        var memberInfos = type.GetMembers(BindingFlags.Public
+                                        | BindingFlags.NonPublic
+                                        | BindingFlags.Instance
+                                        | BindingFlags.Static
+                                        | BindingFlags.DeclaredOnly);
+
+        foreach (var methodInfo in memberInfos)
+        {
+            var remoteCommand = Attribute.GetCustomAttribute(methodInfo, typeof(RemoteCommand)) as RemoteCommand;
+
+            if (remoteCommand == null)
+            {
+                continue;
+            }
+
+            remoteCommand.Initialize(instance, methodInfo);
+
+            AddCommand(remoteCommand);
+        }
+    }
+
+    public virtual object Command(params object[] parameters)
+    {
+        var id = parameters[0].ToString();
+
+        if (!_commands.TryGetValue(id, out var command))
+        {
+            Debug.Log($"Command ID '{id}' is not found.");
+            return null;
+        }
+        try
+        {
+            parameters = parameters.Skip(1).ToArray();
+            return command.Invoke(parameters);
+        }
+        catch (Exception exception)
+        {
+            Debug.Log($"Invoke '{command}' is failed.\n{exception}");
+            return null;
+        }
+    }
+
+    public bool AddCommand(RemoteCommand command)
+    {
+        if (!command.IsInitialized)
+        {
+            Debug.Log("command is not initialized.");
+            return false;
+        }
+
+        if (!_commands.TryAdd(command.ID, command))
+        {
+            Debug.Log($"Conflict Command ID: {_commands[command.ID]}, {command}");
+            return false;
+        }
+
+        return true;
+    }
+
+    #endregion Method
+}}
